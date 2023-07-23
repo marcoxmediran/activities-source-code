8abbf162a9962e0ded6050522436ce26adfe2d9f
marcoxmediran <marcoxmediran@gmail.com>
2023-07-23 13:15:26 +0800

    added missing output

--- comprog/1-2/file-handling/exercise5.c
+++ comprog/1-2/file-handling/exercise5.c
@@ -21,6 +21,7 @@ int get_int(string message);
 int main(void) {
     // local variables
     string file_name = get_string("Input the file name to be opened: ");
+    string read_line = malloc(MAX_LENGTH);
     const int APPEND_LINE_COUNT = get_int("Input the number of lines to be writted: ");
     FILE *fp = fopen(file_name, "a");
     fgetc(stdin);
@@ -29,9 +30,17 @@ int main(void) {
     for (int i = 0; i < APPEND_LINE_COUNT; i++) {
         fprintf(fp, "%s\n", get_string(""));
     }
+    fclose(fp);
+    fp = fopen(file_name, "r");
+    printf("\nThe content of the file %s is:\n", file_name);
+    while(!feof(fp)) {
+        fgets(read_line, MAX_LENGTH, fp); 
+        printf("%s", read_line);
+    }
 
     // program ran successfully
     free(file_name);
+    free(read_line);
     fclose(fp);
     return 0;
 }
